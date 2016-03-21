098fcbe41b397810acfc902108eb29b3a3624497
Allysson dos Santos <aallysson0@gmail.com>
2016-03-20 22:53:00 -0300

    Arrumando botao voltar para aparecer apenas se nao estiver na home, autor e comentarios

--- javascripts/spa.js
+++ javascripts/spa.js
@@ -1,4 +1,16 @@
 void function(){
+
+	function buttonBack() {
+		if (window.location.pathname.length > 2) {
+			$(".blog-back-button").css("display", "inline-block");
+			$(".blog-menu-button").css("right", "15px");
+			$(".blog-menu-button").css("left", "");
+		} else {
+			$(".blog-back-button").css("display", "none");
+			$(".blog-menu-button").css("left", "22px");
+			$(".blog-menu-button").css("right", "");
+		}
+	}
 	$(document).trigger('pageload', this.href);
 
 	if (!('pushState' in window.history)) return;
@@ -18,8 +30,9 @@ void function(){
 		$page.load(url + ' main', function() {
 			$(document).trigger('pageload', this.href);
 		});
+		buttonBack();
 	}
-
+	buttonBack();
 	$body.on('click', 'a', function(event){
 		var href = this.getAttribute('href');
 		if (href.indexOf('#') == 0 || href.indexOf(':') >= 0) return;
@@ -30,6 +43,18 @@ void function(){
 		window.history.pushState({push:true}, '', href);
 
 		load(href);
+		/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+		var disqus_developer = 1;
+		var disqus_shortname = 'allyssonme'; // required: replace example with your forum shortname
+		// var disqus_developer = 1; // Comment out when the site is live
+		var disqus_identifier = "{{ page.url }}";
+
+		/* * * DON'T EDIT BELOW THIS LINE * * */
+		(function() {
+				var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+				dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+				(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+		})();
 
 		event.preventDefault();
 	});
@@ -75,8 +100,9 @@ void function(){
 		}
 	}
 
-self.addEventListener("notificationclick", function(event) {
-  clients.openWindow('/index.html');
-});
+	self.addEventListener("notificationclick", function(event) {
+	  clients.openWindow('/index.html');
+	});
+
 
 }();
