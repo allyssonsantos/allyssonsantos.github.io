89418574e79dc212375452951c94c6ac37060bbd
Allysson dos Santos <aallysson0@gmail.com>
2016-03-23 21:44:40 -0300

    Teste php2

--- push.php
+++ push.php
@@ -1,5 +1,15 @@
 <?php
 
 echo "teste";
-
  ?>
+
+ <!DOCTYPE html>
+ <html>
+   <head>
+     <meta charset="utf-8">
+     <title>teste</title>
+   </head>
+   <body>
+     Testando tudo
+   </body>
+ </html>
