82a81afd509625fa7ece6e8eb613b5eb24212d77
Allysson dos Santos <aallysson0@gmail.com>
2016-03-24 07:57:09 -0300

    Adicionando Cache

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,71 @@
+var files = [
+  "index.html",
+  "javascripts/jquery-git.min.js",
+  "worker.js",
+  "javascripts/spa.js",
+  "javascripts/busca.js",
+  "imgs/icon.png",
+  "css/main.css",
+  "imgs/teste.png",
+  "imgs/allysson.jpg",
+  "//allyssonme.disqus.com/embed.js",
+  "//a.disquscdn.com/next/embed/alfie.f51946af45e0b561c60f768335c9eb79.js",
+  "html5/2016/02/02/teste-post-77777.html",
+  "imgs/svgs/menu.svg",
+  "imgs/svgs/arrows.svg",
+  "imgs/svgs/github.svg",
+  "imgs/svgs/linkedin.svg",
+  "imgs/svgs/facebook.svg",
+  "imgs/svgs/email.svg",
+  "html5/2016/02/02/teste-post-55555.html",
+  "css/2016/02/01/post-de-teste.html"
+];
+// dev only
+if (typeof files == 'undefined') {
+  var files = [];
+} else {
+  files.push('./');
+}
+
+var CACHE_NAME = 'allysson';
+
+self.addEventListener('activate', function(event) {
+  console.log('[SW] Activate');
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (CACHE_NAME.indexOf(cacheName) == -1) {
+            console.log('[SW] Delete cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener('install', function(event) {
   self.skipWaiting();
   console.log('Installed', event);
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return Promise.all(
+        files.map(function(file) {
+          return cache.add(file);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log('[SW] fetch ' + event.request.url)
+  event.respondWith(
+    caches.match(event.request).then(function(response){
+      return response || fetch(event.request.clone());
+    })
+  );
 });
 
 self.addEventListener('activate', function(event) {
