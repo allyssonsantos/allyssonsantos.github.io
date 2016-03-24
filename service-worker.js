5a20c02e805235f60d99f80b22a1ca45778d567e
Allysson dos Santos <aallysson0@gmail.com>
2016-03-23 21:03:07 -0300

    Arrumando service worker, manifest, worker e mais algumas coisas de push notifications

--- service-worker.js
+++ service-worker.js
@@ -1,110 +1,44 @@
-self.addEventListener("push", function() {
-  console.log("push received");
-
-  var httpHeaders = new Headers();
-  httpHeaders.append('pragma', 'no-cache');
-  httpHeaders.append('cache-control', 'no-cache');
-
-  var fetchInit = {
-    method: 'GET',
-    headers: httpHeaders,
-  };
-
-  fetch("http://localhost:4000/push/latest.json", fetchInit).then(function(res) {
-    res.json().then(function(data) {
-
-      //Show notification
-      if ('Notification' in window) {
-  			console.log ("aeee");
-  		}
-
-  		if (Notification.permission=="granted") {
-  			self.registration.showNotification(notificationData.data.title, {
-  				body: notificationData.data.body,
-  				icon: '/imgs/icon.png'
-  			});
-  		} else {
-  			Notification.requestPermission(function(permission) {
-  				if (permission=='granted') {
-  						self.registration.showNotification(notificationData.data.title, {
-  							body: notificationData.data.body,
-  							icon: '/imgs/icon.png'
-  						});
-  				}
-  			});
-  		}
-  		self.addEventListener('notificationclick', function(e) {
-  			if (clients.openWindow) {
-  				clients.openWindow(notificationData.data.url);
-  			}
-  		});
-    });
-  });
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+  console.log('Installed', event);
 });
 
-// var files = [
-//   "index.html",
-//   "imgs/icon.png",
-//   "imgs/svgs/arrows.svg",
-//   "imgs/svgs/menu.svg",
-//   "imgs/svgs/github.svg",
-//   "imgs/svgs/linkedin.svg",
-//   "imgs/svgs/facebook.svg",
-//   "imgs/svgs/email.svg",
-//   "imgs/teste.png",
-//   "javascripts/jquery-git.min.js",
-//   "javascripts/spa.js",
-//   "javascripts/busca.js",
-//   "css/main.css"
-// ];
-// // dev only
-// if (typeof files == 'undefined') {
-//   var files = [];
-// } else {
-//   files.push('./');
-// }
-//
-// var CACHE_NAME = 'allyssonblog-v1';
-//
-// self.addEventListener('activate', function(event) {
-//   console.log('[SW] Activate');
-//   event.waitUntil(
-//     caches.keys().then(function(cacheNames) {
-//       return Promise.all(
-//         cacheNames.map(function(cacheName) {
-//           if (CACHE_NAME.indexOf(cacheName) == -1) {
-//             console.log('[SW] Delete cache:', cacheName);
-//             return caches.delete(cacheName);
-//           }
-//         })
-//       );
-//     })
-//   );
-// });
-//
-// self.addEventListener('install', function(event){
-//   console.log('[SW] Install');
-//   event.waitUntil(
-//     caches.open(CACHE_NAME).then(function(cache) {
-//       return Promise.all(
-//       	files.map(function(file){
-//       		return cache.add(file);
-//       	})
-//       );
-//     })
-//   );
-// });
-//
-// self.addEventListener('fetch', function(event) {
-//   console.log('[SW] fetch ' + event.request.url);
-//   event.respondWith(
-//     caches.match(event.request).then(function(response){
-//       return response || fetch(event.request.clone());
-//     })
-//   );
-// });
-//
-// self.addEventListener('notificationclick', function(event) {
-//   console.log('On notification click: ', event);
-//   clients.openWindow('/');
-// });
+self.addEventListener('activate', function(event) {
+  console.log('Activated', event);
+});
+
+self.addEventListener('push', function(event) {
+  console.log('Push message', event);
+
+  var title = 'Push message';
+
+  event.waitUntil(
+    self.registration.showNotification(title, {
+      'body': 'The Messadasdsa',
+      'icon': 'imgs/icon.png'
+    }));
+});
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('Notification click: tag', event.notification.tag);
+  event.notification.close();
+  var url = 'https://youtu.be/gYMkEMCHtJ4';
+  event.waitUntil(
+    clients.matchAll({
+      type: 'window'
+    })
+    .then(function(windowClients) {
+      console.log('WindowClients', windowClients);
+      for (var i = 0; i < windowClients.length; i++) {
+        var client = windowClients[i];
+        console.log('WindowClient', client);
+        if (client.url === url && 'focus' in client) {
+          return client.focus();
+        }
+      }
+      if (clients.openWindow) {
+        return clients.openWindow(url);
+      }
+    })
+  );
+});
