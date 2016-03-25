c353bd583075072e2503925e38da71a04f553d0b
Beatriz Figueiredo <beatrizfigueiredo@Beatrizs-Mac-mini.local>
2016-03-25 18:24:44 -0300

    Alterando mensagem push

--- service-worker.js
+++ service-worker.js
@@ -10,11 +10,11 @@ self.addEventListener('activate', function(event) {
 self.addEventListener('push', function(event) {
   console.log('Push message', event);
 
-  var title = 'Push message';
+  var title = 'Titulo da notificacao';
 
   event.waitUntil(
     self.registration.showNotification(title, {
-      'body': 'The Messadasdsa',
+      'body': 'Testando descricao da push',
       'icon': 'imgs/icon.png'
     }));
 });
