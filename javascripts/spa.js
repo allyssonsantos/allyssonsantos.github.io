d57dd7562f147aced668285bfb24c9d34121c673
Allysson dos Santos <allysson@Mac-mini-de-Allysson.local>
2016-06-16 22:13:03 -0300

    Javascript Basics 1 - Escopos de variáveis

--- javascripts/spa.js
+++ javascripts/spa.js
@@ -49,7 +49,7 @@ void function(){
 				var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
 				dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
 				(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-		
+
 			/* * * Disqus Reset Function * * */
 		    var reset = function (newIdentifier, newUrl, newTitle, newLanguage) {
 		        DISQUS.reset({
