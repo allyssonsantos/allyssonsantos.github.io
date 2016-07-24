c9495e5964c3304dca6870bf379af666c3c4c6c5
Beatriz Figueiredo <beatrizfigueiredo@Beatrizs-Mac-mini.local>
2016-07-24 10:20:54 -0300

    Retirando Jquery e continuando post do starwars

--- javascripts/spa.js
+++ javascripts/spa.js
@@ -1,65 +1,61 @@
-void function(){
+(function () {
+    'use strict';
 
-	//Mostrar e esconder o header
-	if ($(window).width() < 768) {
-		var ultimoScroll = 0,
-			header = $(".blog-header");
-		$(window).scroll(function () {
-			var posicaoAtual = $(this).scrollTop();
-			if (posicaoAtual > ultimoScroll && posicaoAtual > 100) {
-				header.removeClass("blog-header--show");
-				header.addClass("blog-header--hide");
-			} else if (posicaoAtual == 0) {
-				header.removeClass("blog-header--hide");
-				header.addClass("blog-header--show");
-			} else {
-				header.removeClass("blog-header--hide");
-				header.addClass("blog-header--show");
-			}
-			ultimoScroll = posicaoAtual;
-		});
-	}
+    var toggleHeader,
+        header,
+        posicaoAtual,
+        ultimoScroll = 0,
+        btnBack = document.querySelector(".blog-back-button");
 
-	//Adicionar botão de voltar e alterar posicao do menu
-	if (window.location.pathname.length > 2) {
-		$(".blog-back-button").css("display", "inline-block");
-		$(".blog-menu-button").css("right", "0");
-		$(".blog-menu-button").css("left", "");
-	} else {
-		$(".blog-back-button").css("display", "none");
-		$(".blog-menu-button").css("left", "0");
-		$(".blog-menu-button").css("right", "");
-	}
+    toggleHeader = function () {
+        header = document.querySelector(".blog-header");
+        posicaoAtual = window.scrollY;
 
-	//Mostrar e esconder links do menu
-	var $blogLinksItem = $(".blog-links-item");
-	$blogLinksItem.click(function() {
-		$("#blog-menu-controller").prop("checked", false);
-	});
+        if (posicaoAtual > ultimoScroll && posicaoAtual > 100) {
+            header.classList.remove("blog-header--show");
+            header.classList.add("blog-header--hide");
+        } else {
+            header.classList.remove("blog-header--hide");
+            header.classList.add("blog-header--show");
+        }
+        ultimoScroll = posicaoAtual;
+    };
 
-		/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-		var disqus_developer = 1;
-		var disqus_shortname = 'allyssonme'; // required: replace example with your forum shortname
-		// var disqus_developer = 1; // Comment out when the site is live
-		var disqus_identifier = "{{ page.url }}";
-		var disqus_url = "{{ page.url }}";
+    if (window.innerWidth < 768) {
+        window.addEventListener("scroll", toggleHeader);
+    }
 
-		/* * * DON'T EDIT BELOW THIS LINE * * */
-		(function() {
-				var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-				dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-				(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    if (window.location.pathname.length > 2) {
+        btnBack.className = "blog-back-button--show";
+    }
+})();
 
-			/* * * Disqus Reset Function * * */
-		    var reset = function (newIdentifier, newUrl, newTitle, newLanguage) {
-		        DISQUS.reset({
-		            reload: true,
-		            config: function () {
-		                this.page.url = "{{ page.url }}";
-		                this.page.title = "{{ page.title }}";
-		        	}
-		    	});
-		    }
-		})();
+(function() {
+    if (!document.querySelector(".post-comments")) {
+        return;
+    }
+    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+    var disqus_developer = 1;
+    var disqus_shortname = 'allyssonme'; // required: replace example with your forum shortname
+    // var disqus_developer = 1; // Comment out when the site is live
+    var disqus_identifier = "{{ page.url }}";
+    var disqus_url = "{{ page.url }}";
 
-}();
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function() {
+            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+
+        /* * * Disqus Reset Function * * */
+        var reset = function (newIdentifier, newUrl, newTitle, newLanguage) {
+            DISQUS.reset({
+                reload: true,
+                config: function () {
+                    this.page.url = "{{ page.url }}";
+                    this.page.title = "{{ page.title }}";
+                }
+            });
+        }
+    })();
+})();
