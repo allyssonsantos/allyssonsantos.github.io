aadec3bc170cbb2174a28236045764ddfb66cdc6
Allysson Santos <aallysson0@gmail.com>
2020-01-17 20:40:54 -0300

    refactor: Post to a functional component

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -5,56 +5,51 @@ import { MDXRenderer } from 'gatsby-plugin-mdx';
 import Layout from '../components/Layout';
 import SEO from '../components/seo';
 
-class BlogPostTemplate extends React.Component {
-  render() {
-    const post = this.props.data.mdx;
-    const siteTitle = this.props.data.site.siteMetadata.title;
-    const { previous, next } = this.props.pageContext;
+const Post = ({
+  location,
+  data: {
+    mdx: post,
+    site: {
+      siteMetadata: { title: siteTitle },
+    },
+  },
+  pageContext: { previous, next },
+}) => (
+  <Layout location={tlocation} title={siteTitle}>
+    <SEO title={post.frontmatter.title} description={post.excerpt} />
+    <h1>{post.frontmatter.title}</h1>
+    <p>{post.frontmatter.date}</p>
+    <MDXRenderer>{post.body}</MDXRenderer>
+    <hr />
 
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO title={post.frontmatter.title} description={post.excerpt} />
-        <h1>{post.frontmatter.title}</h1>
-        <p
-          style={{
-            display: `block`,
-          }}
-        >
-          {post.frontmatter.date}
-        </p>
-        <MDXRenderer>{post.body}</MDXRenderer>
-        <hr style={{}} />
+    <ul
+      style={{
+        display: `flex`,
+        flexWrap: `wrap`,
+        justifyContent: `space-between`,
+        listStyle: `none`,
+        padding: 0,
+      }}
+    >
+      <li>
+        {previous && (
+          <Link to={previous.fields.slug} rel="prev">
+            ← {previous.frontmatter.title}
+          </Link>
+        )}
+      </li>
+      <li>
+        {next && (
+          <Link to={next.fields.slug} rel="next">
+            {next.frontmatter.title} →
+          </Link>
+        )}
+      </li>
+    </ul>
+  </Layout>
+);
 
-        <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
-      </Layout>
-    );
-  }
-}
-
-export default BlogPostTemplate;
+export default Post;
 
 export const pageQuery = graphql`
   query($slug: String!) {
