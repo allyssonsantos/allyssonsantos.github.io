af1ea057752a097b2c6b3d190fbc8a9f3a525af8
Allysson Santos <aallysson0@gmail.com>
2020-06-22 11:15:26 -0300

    feat(Header): new blog header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Link } from 'gatsby';
-import styled, { css } from 'styled-components';
-import { withFade } from '@frigobar/animation';
+import styled, { css, withTheme } from 'styled-components';
+
+import { Box, Link } from '.';
 
 const Wrapper = styled.header`
   height: 526px;
@@ -37,42 +37,15 @@ const Nav = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  margin-bottom: 55px;
 `;
 
 const Menu = styled.div`
   font-size: 0.875rem;
 
-  a {
-    ${({
-      theme: {
-        colors: { white },
-      },
-    }) => css`
-      color: ${white};
-    `}
-  }
-
   a + a {
     margin-left: 32px;
   }
-`;
-
-const Logo = styled.h3`
-  display: inline-block;
-
-  padding: 10px 0;
-
-  font-size: 0.875rem;
-  font-family: 'Roboto', sans-serif;
-  font-weight: normal;
-
-  ${({
-    theme: {
-      colors: { grays },
-    },
-  }) => css`
-    color: ${grays[1]};
-  `}
 `;
 
 const Highlight = styled.div`
@@ -82,21 +55,6 @@ const Highlight = styled.div`
 
 const Info = styled.div`
   margin-right: 74px;
-`;
-
-const Title = styled.h2`
-  font-size: 44px;
-  margin: 0;
-  margin-top: 30px;
-  margin-bottom: 24px;
-
-  ${({
-    theme: {
-      colors: { grays },
-    },
-  }) => css`
-    color: ${grays[0]};
-  `}
 `;
 
 const Message = styled.p`
@@ -116,28 +74,48 @@ const Image = styled.img`
   width: ${props => props.width}px;
 `;
 
-const Header = () => (
+const Header = ({ theme }) => (
   <Wrapper>
     <Content>
       <Nav>
         <Link to="/">
-          <Logo>allysson</Logo>
+          <Box
+            as="h3"
+            display="inline-block"
+            size="0.875rem"
+            color={theme.colors.grays[1]}
+          >
+            allysson
+          </Box>
         </Link>
 
         <Menu>
-          <Link to="/blog">Blog</Link>
-          <Link to="/about">Sobre mim</Link>
+          <Link to="/blog" color={theme.colors.white} weight="normal">
+            Blog
+          </Link>
+          <Link to="/about" color={theme.colors.white} weight="normal">
+            Sobre mim
+          </Link>
         </Menu>
       </Nav>
     </Content>
     <Content>
       <Highlight>
         <Info>
-          <Title>E aí, Beleza?</Title>
-          <Message>
+          <Box
+            as="h2"
+            size="2.75rem"
+            mt={30}
+            mb={24}
+            color={theme.colors.grays[0]}
+            weight="bold"
+          >
+            E aí, Beleza?
+          </Box>
+          <Box as="p" lh="24px">
             Sou o Allysson, desenvolvedor Front End no Gympass e vim trazer mais
             conteúdo em português sobre desenvolvimento.
-          </Message>
+          </Box>
         </Info>
         <Art>
           <Image width={387} right={60} top={0} src="/browser.png" />
@@ -148,4 +126,4 @@ const Header = () => (
   </Wrapper>
 );
 
-export default withFade(Header);
+export default withTheme(Header);
