564fe4d30eb190c7f15b81f4069d9f77dcec18b0
Allysson Santos <aallysson0@gmail.com>
2020-06-22 11:17:33 -0300

    feat(theme): create new theme

---
+++ src/utils/theme/normal.js
@@ -0,0 +1,23 @@
+const light = {
+  sizes: {
+    maxWidth: 910,
+  },
+  radius: 8,
+  colors: {
+    primary: '#6AA79F',
+    secondary: '#596E87',
+    bg: '#F6F6F6',
+    grays: ['#2C2C2C', '#8C8C8C', '#AEAEAE', '#E5E5E5'],
+    white: '#fff',
+    tags: {
+      javascript: '#BC7F7E',
+      designsystem: '#BC7F7E',
+      styledcomponents: '#BC7F7E',
+      react: '#BC7F7E',
+      css: '#BC7F7E',
+      web: '#596E87',
+    },
+  },
+};
+
+export default light;
