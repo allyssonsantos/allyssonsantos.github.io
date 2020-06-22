3d7dd6895c54744076b86ed1a74d4a3bccc776a0
Allysson Santos <aallysson0@gmail.com>
2020-06-22 10:13:13 -0300

    feat(Header): create new header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,35 +4,147 @@ import styled, { css } from 'styled-components';
 import { withFade } from '@frigobar/animation';
 
 const Wrapper = styled.header`
-  max-width: 740px;
-  margin: 0 auto;
-`;
-
-const Title = styled.h2`
-  display: inline-block;
-
-  margin: 20px 10px 0;
-  padding: 10px 0;
-
-  font-size: 1.1rem;
-  font-family: 'Roboto', sans-serif;
-
-  text-decoration: none;
+  height: 526px;
 
   ${({
     theme: {
-      colors: { primary },
+      colors: { white, secondary },
     },
-  }) => `
-    color: ${primary};
+  }) => css`
+    background: linear-gradient(
+      to right,
+      ${white} 0%,
+      53%,
+      ${secondary} 53% 100%
+    );
   `}
 `;
 
-const Header = ({ title }) => (
+const Content = styled.div`
+  margin: 0 auto;
+  padding: 10px;
+
+  ${({
+    theme: {
+      sizes: { maxWidth },
+    },
+  }) => css`
+    max-width: ${maxWidth}px;
+  `}
+`;
+
+const Nav = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Menu = styled.div`
+  font-size: 0.875rem;
+
+  a {
+    ${({
+      theme: {
+        colors: { white },
+      },
+    }) => css`
+      color: ${white};
+    `}
+  }
+
+  a + a {
+    margin-left: 32px;
+  }
+`;
+
+const Logo = styled.h3`
+  display: inline-block;
+
+  padding: 10px 0;
+
+  font-size: 0.875rem;
+  font-family: 'Roboto', sans-serif;
+  font-weight: normal;
+
+  ${({
+    theme: {
+      colors: { grays },
+    },
+  }) => css`
+    color: ${grays[1]};
+  `}
+`;
+
+const Highlight = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Info = styled.div`
+  margin-right: 74px;
+`;
+
+const Title = styled.h2`
+  font-size: 44px;
+  margin: 0;
+  margin-top: 30px;
+  margin-bottom: 24px;
+
+  ${({
+    theme: {
+      colors: { grays },
+    },
+  }) => css`
+    color: ${grays[0]};
+  `}
+`;
+
+const Message = styled.p`
+  line-height: 24px;
+`;
+
+const Art = styled.div`
+  position: relative;
+  width: 100%;
+`;
+
+const Image = styled.img`
+  position: absolute;
+  top: ${props => props.top}px;
+  right: ${props => props.right}px;
+
+  width: ${props => props.width}px;
+`;
+
+const Header = () => (
   <Wrapper>
-    <Link to="/">
-      <Title>{title}</Title>
-    </Link>
+    <Content>
+      <Nav>
+        <Link to="/">
+          <Logo>allysson</Logo>
+        </Link>
+
+        <Menu>
+          <Link to="/blog">Blog</Link>
+          <Link to="/about">Sobre mim</Link>
+        </Menu>
+      </Nav>
+    </Content>
+    <Content>
+      <Highlight>
+        <Info>
+          <Title>E aí, Beleza?</Title>
+          <Message>
+            Sou o Allysson, desenvolvedor Front End no Gympass e vim trazer mais
+            conteúdo em português sobre desenvolvimento.
+          </Message>
+        </Info>
+        <Art>
+          <Image width={387} right={60} top={0} src="/browser.png" />
+          <Image width={234} right={80} top={63} src="/allysson.svg" />
+        </Art>
+      </Highlight>
+    </Content>
   </Wrapper>
 );
 
