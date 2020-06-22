006cbaeff46f4627333cd79879ce89d56920f396
Allysson Santos <aallysson0@gmail.com>
2020-06-22 11:17:09 -0300

    chore(components): export new components

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,29 @@
 import Code from './Code';
-import Paragraph from './Paragraph';
+import Box from './Box';
 import InlineCode from './InlineCode';
 import { Ul, Ol, Li } from './List';
 import Layout from './Layout';
+import Title from './Title';
+import Card from './Card';
+import Tag from './Tag';
+import Button from './Button';
+import Repo from './Repo';
+import Footer from './Footer';
+import Link from './Link';
 
-export { Code, Paragraph, InlineCode, Ul, Ol, Li, Layout };
+export {
+  Code,
+  Box,
+  InlineCode,
+  Ul,
+  Ol,
+  Li,
+  Layout,
+  Title,
+  Card,
+  Tag,
+  Button,
+  Repo,
+  Footer,
+  Link,
+};
