2c65f97e043bbfce74306387cdce1714d55b5c24
Allysson Santos <aallysson0@gmail.com>
2020-08-12 10:30:10 -0300

    refactor(Box): update to use transient props

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components';
 import { normal } from '../utils/theme';
@@ -8,47 +9,47 @@ const Box = styled.div`
 
   line-height: 28px;
 
-  ${({ mt, mr, mb, ml, lh, color, size, weight, display, textAlign }) => css`
-    display: ${display};
+  ${({ $mt, $mr, $mb, $ml, $lh, $color, $size, $weight, $display, $textAlign }) => css`
+    display: ${$display};
 
-    margin-top: ${mt}px;
-    margin-right: ${mr}px;
-    margin-bottom: ${mb}px;
-    margin-left: ${ml}px;
+    margin-top: ${$mt}px;
+    margin-right: ${$mr}px;
+    margin-bottom: ${$mb}px;
+    margin-left: ${$ml}px;
 
-    line-height: ${lh};
+    line-height: ${$lh};
 
-    color: ${color};
-    font-size: ${size};
-    font-weight: ${weight};
-    text-align: ${textAlign};
+    color: ${$color};
+    font-size: ${$size};
+    font-weight: ${$weight};
+    text-align: ${$textAlign};
   `}
 `;
 
 Box.propTypes = {
-  mt: PropTypes.number,
-  mr: PropTypes.number,
-  mb: PropTypes.number,
-  ml: PropTypes.number,
-  lh: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-  color: PropTypes.string,
-  size: PropTypes.string,
-  weight: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-  display: PropTypes.string,
-  textAlign: PropTypes.string,
+  $mt: PropTypes.number,
+  $mr: PropTypes.number,
+  $mb: PropTypes.number,
+  $ml: PropTypes.number,
+  $lh: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  $color: PropTypes.string,
+  $size: PropTypes.string,
+  $weight: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  $display: PropTypes.string,
+  $textAlign: PropTypes.string,
 };
 
 Box.defaultProps = {
-  mt: 0,
-  mr: 0,
-  mb: 0,
-  ml: 0,
-  lh: 'initial',
-  color: normal.colors.grays[0],
-  size: '1rem',
-  weight: 'normal',
-  display: 'block',
-  textAlign: 'left',
+  $mt: 0,
+  $mr: 0,
+  $mb: 0,
+  $ml: 0,
+  $lh: 'initial',
+  $color: normal.colors.grays[0],
+  $size: '1rem',
+  $weight: 'normal',
+  $display: 'block',
+  $textAlign: 'left',
 };
 
 export default Box;
