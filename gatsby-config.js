a96c7fee766a6acdedf464ed37c44393ee7afbd4
Allysson Santos <aallysson0@gmail.com>
2020-11-09 13:31:18 -0300

    feat(analytics): add tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,7 +58,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-75814657-1`,
       },
     },
     {
