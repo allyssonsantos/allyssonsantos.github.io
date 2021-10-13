8f66a96b3eb23c2b63489aed416cbc6287c41759
Allysson Santos <aallysson0@gmail.com>
2021-10-12 23:58:36 -0300

    feat(home): add home styled components

---
+++ src/components/Home.jsx
@@ -0,0 +1,147 @@
+import styled, { css } from 'styled-components';
+import rem from '@utils/rem';
+
+const Title = styled.h1(
+  ({ theme }) => css`
+    font-size: ${rem(46)};
+
+    margin: 0;
+
+    color: ${theme.colors.neutral[800]};
+  `
+);
+
+const SubTitle = styled(Title).attrs({ as: 'h2' })`
+  font-size: ${rem(18)};
+  font-weight: normal;
+
+  margin-bottom: 12px;
+`;
+
+const Description = styled.p`
+  font-size: ${rem(14)};
+  font-weight: normal;
+
+  margin: 0;
+  margin-top: 8px;
+`;
+
+const Posts = styled.section(
+  ({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+
+    margin-top: 64px;
+  `
+);
+
+const Post = styled.article(
+  ({ theme }) => css`
+    padding-bottom: ${theme.spacings.xlarge}px;
+
+    transition: transform 0.3s ease;
+
+    & + & {
+      padding-top: ${theme.spacings.xlarge}px;
+
+      border-top: ${theme.borders.tiny}px solid ${theme.colors.neutral[200]};
+    }
+
+    &:hover {
+      transform: scale(1.02);
+    }
+  `
+);
+
+const PostTitle = styled.h3(
+  ({ theme }) => css`
+    font-size: ${rem(24)};
+
+    margin: 0;
+
+    color: ${theme.colors.neutral[800]};
+  `
+);
+
+const PostDescription = styled.p(
+  ({ theme }) => `
+  font-size: ${rem(16)};
+  line-height: ${rem(22)};
+
+  color: ${theme.colors.neutral[800]};
+
+  margin: 0;
+  margin-top: 8px;
+`
+);
+
+const Projects = styled.section(
+  ({ theme }) => css`
+    margin-top: ${theme.spacings.xxlarge}px;
+  `
+);
+
+const Repos = styled.div(
+  ({ theme }) => css`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+  `
+);
+
+const Project = styled.a(
+  ({ theme }) => css`
+    display: flex;
+    flex-basis: 200px;
+    flex-direction: column;
+    flex-grow: 1;
+    justify-content: space-between;
+
+    padding: ${theme.spacings.small}px;
+
+    transition: transform 0.3s ease;
+    text-decoration: none;
+
+    border: ${theme.borders.tiny}px solid ${theme.colors.neutral[200]};
+    border-radius: ${theme.radius[2]}px;
+
+    &:hover {
+      transform: scale(1.02);
+    }
+  `
+);
+
+const ProjectTitle = styled(PostTitle)`
+  margin-bottom: 12px;
+
+  text-transform: lowercase;
+`;
+const ProjectDescription = styled(PostDescription)`
+  font-size: ${rem(14)};
+`;
+const ProjectStars = styled.span`
+  font-size: ${rem(12)};
+
+  display: flex;
+  align-items: center;
+  align-self: flex-end;
+  gap: 10px;
+
+  margin-top: 10px;
+`;
+
+export {
+  Title,
+  SubTitle,
+  Description,
+  Posts,
+  Post,
+  PostTitle,
+  PostDescription,
+  Projects,
+  Repos,
+  Project,
+  ProjectTitle,
+  ProjectDescription,
+  ProjectStars,
+};
