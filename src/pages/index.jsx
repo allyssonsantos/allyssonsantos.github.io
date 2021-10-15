beb4235489dddbf6d87f8d4a76399278efcf0b35
Allysson Santos <aallysson0@gmail.com>
2021-10-15 08:24:47 -0300

    refactor(home): get github content from gatsby query

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -26,83 +26,71 @@ const Home = ({
       siteMetadata: { title: siteTitle },
     },
     allMdx: { edges: posts },
+    github: { repositories },
   },
-}) => {
-  const [projects, setProjects] = useState([]);
+}) => (
+  <Layout location={location} title={siteTitle} full>
+    <SEO
+      title="Home"
+      keywords={[
+        'blog',
+        'gatsby',
+        'javascript',
+        'react',
+        'styled-components',
+        'design-system',
+        'components',
+      ]}
+    />
+    <section>
+      <Title>Allysson Santos</Title>
+      <Description as="h2">
+        Frontend Developer no <strong>Olist</strong>
+      </Description>
+    </section>
+    <Posts>
+      <SubTitle>Posts recentes</SubTitle>
+      {posts.map(({ node }) => {
+        const title = node.frontmatter.title || node.fields.slug;
+        return (
+          <Post>
+            <PostTitle>{title}</PostTitle>
+            <PostDescription>{node.frontmatter.description}</PostDescription>
+          </Post>
+        );
+      })}
+    </Posts>
+    <Projects>
+      <SubTitle>Projetos</SubTitle>
+      <Repos>
+        {repositories.map(({ name, description, stars, url }) => (
+          <Project href={url}>
+            <div>
+              <ProjectTitle>{name}</ProjectTitle>
+              <ProjectDescription>{description}</ProjectDescription>
+            </div>
+            <ProjectStars>
+              {stars}
 
-  useEffect(() => {
-    const fetchProjects = async () => {
-      const result = await getPersonalProjects();
-      setProjects(result);
-    };
-
-    fetchProjects();
-  }, []);
-
-  return (
-    <Layout location={location} title={siteTitle} full>
-      <SEO
-        title="Home"
-        keywords={[
-          'blog',
-          'gatsby',
-          'javascript',
-          'react',
-          'styled-components',
-          'design-system',
-          'components',
-        ]}
-      />
-      <section>
-        <Title>Allysson Santos</Title>
-        <Description as="h2">
-          Frontend Developer no <strong>Olist</strong>
-        </Description>
-      </section>
-      <Posts>
-        <SubTitle>Posts recentes</SubTitle>
-        {posts.map(({ node }) => {
-          const title = node.frontmatter.title || node.fields.slug;
-          return (
-            <Post>
-              <PostTitle>{title}</PostTitle>
-              <PostDescription>{node.frontmatter.description}</PostDescription>
-            </Post>
-          );
-        })}
-      </Posts>
-      <Projects>
-        <SubTitle>Projetos</SubTitle>
-        <Repos>
-          {projects.map(({ name, description, stars, url }) => (
-            <Project href={url}>
-              <div>
-                <ProjectTitle>{name}</ProjectTitle>
-                <ProjectDescription>{description}</ProjectDescription>
-              </div>
-              <ProjectStars>
-                {stars}
-
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  width="16"
-                  height="16"
-                  fill="#333"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M12.672.668a.75.75 0 00-1.345 0L8.27 6.865l-6.838.994a.75.75 0 00-.416 1.279l4.948 4.823-1.168 6.811a.75.75 0 001.088.791L12 18.347l6.117 3.216a.75.75 0 001.088-.79l-1.168-6.812 4.948-4.823a.75.75 0 00-.416-1.28l-6.838-.993L12.672.668z"
-                  />
-                </svg>
-              </ProjectStars>
-            </Project>
-          ))}
-        </Repos>
-      </Projects>
-    </Layout>
-  );
-};
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                width="16"
+                height="16"
+                fill="#333"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M12.672.668a.75.75 0 00-1.345 0L8.27 6.865l-6.838.994a.75.75 0 00-.416 1.279l4.948 4.823-1.168 6.811a.75.75 0 001.088.791L12 18.347l6.117 3.216a.75.75 0 001.088-.79l-1.168-6.812 4.948-4.823a.75.75 0 00-.416-1.28l-6.838-.993L12.672.668z"
+                />
+              </svg>
+            </ProjectStars>
+          </Project>
+        ))}
+      </Repos>
+    </Projects>
+  </Layout>
+);
 
 export default Home;
 
@@ -127,6 +115,14 @@ export const pageQuery = graphql`
             tags
           }
         }
+      }
+    }
+    github {
+      repositories {
+        name
+        description
+        stars
+        url
       }
     }
   }
