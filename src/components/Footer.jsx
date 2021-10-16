9bfda97fa4d65c7653dfb834a7f6c174d40df678
Allysson Santos <aallysson0@gmail.com>
2021-10-15 22:07:13 -0300

    feat(footer): update component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,71 +1,14 @@
-import React from 'react';
-import styled, { withTheme, css } from 'styled-components';
+import styled from 'styled-components';
 
-import { Link, Box } from '.';
+const Socials = styled.a``;
 
-const Wrapper = styled.footer`
-  border: 1px solid #e5e5e5;
-  padding: 24px 10px;
-  margin: 0;
-  margin-top: 62px;
-
-  a + a {
-    margin-left: 36px;
-  }
-
-  ${({ theme: { sizes } }) => css`
-    @media (max-width: ${sizes.breakpoints.md}px) {
-      a {
-        display: block;
-      }
-
-      a + a {
-        margin-left: 0;
-      }
-    }
-  `}
-`;
-
-const Center = styled.div`
-  margin: 0 auto;
-
+const Footer = styled.footer`
   display: flex;
   align-items: center;
   justify-content: space-between;
-
-  ${({ theme: { sizes } }) => css`
-    max-width: ${sizes.maxWidth}px;
-
-    @media (max-width: ${sizes.breakpoints.md}px) {
-      align-items: flex-start;
-    }
-  `}
+  margin-top: ${props => props.theme.spacings.xxxlarge}px;
 `;
 
-const Footer = ({ theme }) => (
-  <Wrapper>
-    <Center>
-      <Box
-        as="span"
-        $color={theme.colors.grays[1]}
-        $size="0.875rem"
-        $display="inline-block"
-      >
-        © Feito por Allysson
-      </Box>
-      <div>
-        <Link as="a" href="https://www.linkedin.com/in/allyssonsantos/">
-          LinkedIn
-        </Link>
-        <Link as="a" href="https://twitter.com/_allyssonsantos">
-          Twitter
-        </Link>
-        <Link as="a" href="https://github.com/allyssonsantos">
-          Github
-        </Link>
-      </div>
-    </Center>
-  </Wrapper>
-);
+Footer.Socials = Socials;
 
-export default withTheme(Footer);
+export default Footer;
