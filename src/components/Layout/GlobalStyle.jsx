0c248d934dbc7bf88604a75df21e5c0a2dcaf6f9
Allysson Santos <aallysson0@gmail.com>
2021-11-18 21:45:14 -0300

    feat(GlobalStyle): add new title and code styles

--- src/components/Layout/GlobalStyle.jsx
+++ src/components/Layout/GlobalStyle.jsx
@@ -1,4 +1,5 @@
 import { createGlobalStyle, css } from 'styled-components';
+import rem from '@utils/rem';
 
 const GlobalStyle = createGlobalStyle(
   ({ theme }) => css`
@@ -9,12 +10,16 @@ const GlobalStyle = createGlobalStyle(
 
     html,
     body {
-      font-family: 'Roboto', serif;
+      font-family: 'Poppins', serif;
 
       margin: 0;
       padding: 0;
 
       background-color: ${theme.colors.neutral[50]};
+
+      @media (max-width: 768px) {
+        font-size: 90%;
+      }
     }
 
     body {
@@ -30,12 +35,37 @@ const GlobalStyle = createGlobalStyle(
       color: ${theme.colors.neutral[900]};
     }
 
-    h2 {
-      margin-top: ${theme.spacings.xxxlarge}px;
+    h2,
+    h3,
+    h4,
+    h5,
+    h6 {
+      margin-top: ${theme.spacings.huge}px;
       margin-bottom: ${theme.spacings.medium}px;
     }
 
+    h2 {
+      font-size: ${rem(32)};
+    }
+
+    h3 {
+      font-size: ${rem(24)};
+    }
+
+    h4 {
+      font-size: ${rem(20)};
+    }
+
+    h5 {
+      font-size: ${rem(18)};
+    }
+
+    h6 {
+      font-size: ${rem(18)};
+    }
+
     p {
+      font-size: ${rem(18)};
       line-height: 1.75;
 
       color: ${theme.colors.neutral[900]};
@@ -44,25 +74,61 @@ const GlobalStyle = createGlobalStyle(
     }
 
     img {
-      width: 100%;
-      max-width: 100%;
+      width: 120%;
+      max-width: calc(100% + 80px);
+
+      margin-left: -${theme.spacings.xxlarge}px;
 
       border: 1px solid ${theme.colors.neutral[200]};
-      border-radius: ${theme.radius[2]}px;
+      border-radius: 0;
+
+      @media (min-width: 768px) {
+        border-radius: ${theme.radius[2]}px;
+      }
     }
 
     pre {
-      padding: ${theme.spacings.small}px;
+      font-size: ${rem(14)};
+      line-height: 1.75;
 
-      border: 1px solid ${theme.colors.neutral[200]};
-      border-radius: ${theme.radius[2]}px;
+      padding: ${theme.spacings.xxlarge}px;
+      margin-left: -${theme.spacings.xxlarge}px;
+      margin-right: -${theme.spacings.xxlarge}px;
+      border-radius: 0;
+
+      overflow-x: auto;
+
+      code {
+        display: inline-block;
+        width: 100%;
+      }
+
+      .highlight-line {
+        background-color: ${theme.colors.primary[50]}60;
+        margin-left: -${theme.spacings.xxlarge}px;
+        margin-right: -${theme.spacings.xxlarge}px;
+        padding-left: ${theme.spacings.xxlarge - 5}px;
+        padding-right: ${theme.spacings.xxlarge}px;
+
+        border-left: 5px solid ${theme.colors.primary[200]};
+      }
+
+      @media (min-width: 768px) {
+        border-radius: ${theme.radius[2]}px;
+      }
     }
 
     blockquote {
       margin: 0;
-      padding-left: ${theme.spacings.xlarge}px;
+      padding: ${theme.spacings.small}px ${theme.spacings.small}px
+        ${theme.spacings.small}px ${theme.spacings.xlarge}px;
+      background-color: ${theme.colors.info[50]};
 
-      border-left: ${theme.borders.medium}px solid ${theme.colors.neutral[200]};
+      border-left: 6px solid ${theme.colors.info[200]};
+
+      p {
+        margin: 0;
+      }
     }
 
     table {
@@ -84,9 +150,7 @@ const GlobalStyle = createGlobalStyle(
     }
 
     a {
-      text-decoration: none;
-
-      color: ${props => props.theme.colors.primary[200]};
+      color: ${props => props.theme.colors.primary[500]};
     }
 
     time {
@@ -95,6 +159,16 @@ const GlobalStyle = createGlobalStyle(
 
     svg {
       fill: ${theme.colors.neutral[900]};
+    }
+
+    li {
+      a {
+        line-height: normal;
+      }
+
+      ::marker {
+        color: ${theme.colors.neutral[900]};
+      }
     }
   `
 );
