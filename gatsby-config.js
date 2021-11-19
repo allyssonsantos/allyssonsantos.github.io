038c8dd4054d3f3850279f119d33c80c363b9f6f
Allysson Santos <aallysson0@gmail.com>
2021-11-18 21:40:57 -0300

    feat: add new typography

--- gatsby-config.js
+++ gatsby-config.js
@@ -143,10 +143,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [
-          `Roboto\:400,400i,500,500i,700,700i,800,800i,900,900i`,
-          `Merriweather\:300,300i,400,400i,700,700i,900,900i`,
-        ],
+        fonts: [`Poppins\:300,300i,400,400i,700,700i`],
         display: 'swap',
       },
     },
