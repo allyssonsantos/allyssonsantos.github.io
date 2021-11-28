9db7e2f0561b3317427c79f7cf1ed7cd027ed773
Allysson Santos <aallysson0@gmail.com>
2021-11-28 13:35:28 -0300

    refactor(layout): update Wrapper styles

--- src/components/Layout/Wrapper.jsx
+++ src/components/Layout/Wrapper.jsx
@@ -1,8 +1,19 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-const Wrapper = styled.div`
-  max-width: 800px;
-  margin: 0 auto;
-`;
+const Wrapper = styled.div(
+  ({ menuOpened }) => css`
+    max-width: 800px;
+    width: calc(100% - 240px);
+    margin: 0 auto;
+    padding-top: ${props => props.theme.spacings.huge}px;
+    padding-left: 16px;
+
+    @media (max-width: 1024px) {
+      width: 100%;
+    }
+
+    ${menuOpened ? 'pointer-events: none;' : ''}
+  `
+);
 
 export default Wrapper;
