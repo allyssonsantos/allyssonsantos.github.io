522534c67d3c7f0132723bb279f9c6e46e16bfa5
Allysson Santos <aallysson0@gmail.com>
2021-11-27 22:52:21 -0300

    feat(animation): add transition link

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,12 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: 'gatsby-plugin-transition-link',
+      options: {
+        layout: require.resolve('./src/components/Layout/Layout.jsx'),
+      },
+    },
+    {
       resolve: 'gatsby-plugin-react-svg',
       options: {
         rule: {
