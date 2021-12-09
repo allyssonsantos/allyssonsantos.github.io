dcc6d4faf85a8760049cfcd742e21212cc8df1b8
Allysson Santos <aallysson0@gmail.com>
2021-12-08 23:30:37 -0300

    feat: create subtitle site component

--- src/components/Elements/index.js
+++ src/components/Elements/index.js
@@ -4,5 +4,6 @@ import InlineCode from './InlineCode';
 import { Ul, Ol, Li } from './List';
 import Title from './Title';
 import Link from './Link';
+import Subtitle from './Subtitle';
 
-export { Code, Img, InlineCode, Ul, Ol, Li, Title, Link };
+export { Code, Img, InlineCode, Ul, Ol, Li, Title, Link, Subtitle };
