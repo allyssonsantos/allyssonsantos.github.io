b8750a4d5c41023a3a0ec9acb4daa30d8a8475f6
Allysson Santos <aallysson0@gmail.com>
2022-01-25 14:18:14 -0300

    chore(gatsby): remove webpack plugin analyzer

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,6 @@ module.exports = {
     },
   },
   plugins: [
-    'gatsby-plugin-webpack-bundle-analyser-v2',
     {
       resolve: 'gatsby-plugin-transition-link',
       options: {
@@ -96,16 +95,15 @@ module.exports = {
         `,
         feeds: [
           {
-            serialize: ({ query: { site, allMdx } }) => {
-              return allMdx.edges.map((edge) => ({
+            serialize: ({ query: { site, allMdx } }) =>
+              allMdx.edges.map((edge) => ({
                 ...edge.node.frontmatter,
                 description: edge.node.excerpt,
                 data: edge.node.frontmatter.date,
                 url: site.siteMetadata.siteUrl + edge.node.fields.slug,
                 guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
                 custom_elements: [{ 'content:encoded': edge.node.body }],
-              }));
-            },
+              })),
 
             query: `
             {
