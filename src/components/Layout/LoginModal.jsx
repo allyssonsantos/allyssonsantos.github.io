7d20cf537f2af3837b2d9d5e4cfd7c52624328a3
Allysson Santos <aallysson0@gmail.com>
2022-01-25 14:22:48 -0300

    feat(loginmodal): add disclaimer

--- src/components/Layout/LoginModal.jsx
+++ src/components/Layout/LoginModal.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 import { Modal } from '@frigobar/core';
 
 import { Subtitle } from '@components/Elements';
@@ -7,36 +8,43 @@ import { Subtitle } from '@components/Elements';
 import { useAuth } from '../../contexts/AuthContext';
 import GoogleBtn from '../SignIn/Google';
 
-const StyledModal = styled(Modal)`
-  section {
-    border: none;
-  }
-  button svg {
-    stroke: ${(props) => props.theme.colors.neutral[900]};
-  }
-`;
-
 const Content = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
-  justify-content: space-evenly;
+  justify-content: space-between;
 
   min-width: 300px;
   min-height: 200px;
+
+  small {
+    display: block;
+
+    text-align: center;
+
+    color: ${(props) => props.theme.colors.neutral[900]};
+  }
 `;
 
 function LoginModal({ onClose, ...props }) {
   const { login } = useAuth();
 
   return (
-    <StyledModal onClose={onClose} {...props}>
+    <Modal onClose={onClose} {...props}>
       <Content>
         <Subtitle as="h3">Escolha uma forma de login:</Subtitle>
         <GoogleBtn onClick={login} />
+        <div>
+          <small>Seu nome será utilizado para exibir em comentários.</small>
+          <small>Você pode deletar seus dados a qualquer momento.</small>
+        </div>
       </Content>
-    </StyledModal>
+    </Modal>
   );
 }
+
+LoginModal.propTypes = {
+  onClose: PropTypes.func.isRequired,
+};
 
 export default LoginModal;
