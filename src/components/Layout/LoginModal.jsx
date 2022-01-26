8e33ee6489572ee67173bb1e362f9742024c6543
Allysson Santos <aallysson0@gmail.com>
2022-01-25 21:40:45 -0300

    refactor(loginmodal): get login from AuthContext module, and add alert in case of errors on login

--- src/components/Layout/LoginModal.jsx
+++ src/components/Layout/LoginModal.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import { Modal } from '@frigobar/core';
+import { Modal, Alert } from '@frigobar/core';
 
 import { Subtitle } from '@components/Elements';
 
-import { useAuth } from '../../contexts/AuthContext';
+import { login } from '../../contexts/AuthContext';
 import GoogleBtn from '../SignIn/Google';
+
+const POPUP_CLOSED_ERROR = 'auth/popup-closed-by-user';
 
 const Content = styled.div`
   display: flex;
@@ -27,13 +29,28 @@ const Content = styled.div`
 `;
 
 function LoginModal({ onClose, ...props }) {
-  const { login } = useAuth();
+  const [error, setError] = useState(false);
+
+  const handleLogin = useCallback(async () => {
+    try {
+      await login();
+    } catch (err) {
+      if (err.code === POPUP_CLOSED_ERROR) {
+        return;
+      }
+
+      setError(err);
+    }
+  }, []);
 
   return (
     <Modal onClose={onClose} {...props}>
       <Content>
         <Subtitle as="h3">Escolha uma forma de login:</Subtitle>
-        <GoogleBtn onClick={login} />
+        <GoogleBtn onClick={handleLogin} />
+        <Alert show={error} type="warning">
+          Ocorreu algum problema com o seu login, tente novamente.
+        </Alert>
         <div>
           <small>Seu nome será utilizado para exibir em comentários.</small>
           <small>Você pode deletar seus dados a qualquer momento.</small>
