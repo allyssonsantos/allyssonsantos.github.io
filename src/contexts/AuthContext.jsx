34e0e8bb59015db8282ffe3436ed307c59dbd2e1
Allysson Santos <aallysson0@gmail.com>
2022-01-25 21:43:27 -0300

    refactor(authcontext): move auth methods to outside Provider and export as single functions

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -28,12 +28,64 @@ import { auth, db } from '../services/firebase';
 const AuthContext = createContext();
 
 function useAuth() {
-  return useContext(AuthContext);
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error('useAuth mst be used within a AuthProvider');
+  }
+  return context;
+}
+
+async function login() {
+  try {
+    const provider = new GoogleAuthProvider();
+    provider.setCustomParameters({
+      prompt: 'select_account',
+    });
+    await signInWithPopup(auth, provider);
+
+    return true;
+  } catch (err) {
+    return Promise.reject(err);
+  }
+}
+
+async function logout() {
+  try {
+    await signOut(auth);
+    navigate('/');
+
+    return true;
+  } catch (err) {
+    return Promise.reject(err);
+  }
+}
+
+async function deleteAccount() {
+  const user = auth.currentUser;
+
+  try {
+    await deleteUser(user);
+
+    const q = query(collection(db, 'comments'), where('uid', '==', user.uid));
+    const querySnapshot = await getDocs(q);
+
+    querySnapshot.forEach((doc) => deleteDoc(doc.ref));
+
+    navigate('/');
+  } catch (err) {
+    if (err) {
+      const provider = new GoogleAuthProvider();
+      provider.setCustomParameters({
+        prompt: 'select_account',
+      });
+
+      reauthenticateWithPopup(user, provider);
+    }
+  }
 }
 
 function AuthProvider({ children }) {
   const [currentUser, setCurrentUser] = useState();
-  const [error, setError] = useState(false);
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
@@ -43,53 +95,9 @@ function AuthProvider({ children }) {
     return unsubscribe;
   }, []);
 
-  async function login() {
-    try {
-      const provider = new GoogleAuthProvider();
-      provider.setCustomParameters({
-        prompt: 'select_account',
-      });
-      await signInWithPopup(auth, provider);
-    } catch (err) {
-      setError(err);
-    }
-  }
-
-  async function logout() {
-    await signOut(auth);
-    navigate('/');
-  }
-
-  async function deleteAccount() {
-    const provider = new GoogleAuthProvider();
-    provider.setCustomParameters({
-      prompt: 'select_account',
-    });
-    const user = auth.currentUser;
-
-    try {
-      await deleteUser(user);
-
-      const q = query(collection(db, 'comments'), where('uid', '==', user.uid));
-      const querySnapshot = await getDocs(q);
-
-      querySnapshot.forEach((doc) => deleteDoc(doc.ref));
-
-      navigate('/');
-    } catch (err) {
-      if (err) {
-        reauthenticateWithPopup(user, provider);
-      }
-    }
-  }
-
   const value = useMemo(
     () => ({
       currentUser,
-      error,
-      login,
-      logout,
-      deleteAccount,
     }),
     [currentUser]
   );
@@ -100,4 +108,4 @@ AuthProvider.propTypes = {
   children: node.isRequired,
 };
 
-export { useAuth, AuthProvider };
+export { useAuth, AuthProvider, login, logout, deleteAccount };
