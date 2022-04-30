a386212ba10f9f189c782fe669a7ccccfce21a09
Allysson <aallysson0@gmail.com>
2022-04-30 21:19:44 +0000

    Updates

--- sw.js
+++ sw.js
@@ -48,11 +48,11 @@ self.__precacheManifest = [
     "url": "0c2b6648-ca34de9959b9fe795694.js"
   },
   {
-    "url": "app-d5683bbf9f7a6f5478cc.js"
+    "url": "app-27ee9c27d711149324cb.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "26c2fb6f983f1541181e037b0e90c2db"
+    "revision": "b4269f87f2159950de026245eb941eeb"
   },
   {
     "url": "polyfill-772e4c797a0341636cb6.js"
@@ -151,7 +151,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-d5683bbf9f7a6f5478cc.js`))) {
+  if (!resources || !(await caches.match(`/app-27ee9c27d711149324cb.js`))) {
     return await fetch(event.request)
   }
 
