079c9c9bc64231719e969058796b5ed2089f0d5a
Allysson Santos <aallysson0@gmail.com>
2022-12-07 22:22:21 -0300

    feat: create active link component

---
+++ src/components/ActiveLink/ActiveLink.tsx
@@ -0,0 +1,57 @@
+import { useRouter } from 'next/router';
+import Link, { LinkProps } from 'next/link';
+import React, { PropsWithChildren, useState, useEffect } from 'react';
+
+interface ActiveLinkProps
+  extends LinkProps,
+    Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, 'href'> {
+  href?: string;
+  className?: string;
+  activeClassName: string;
+}
+
+const ActiveLink = ({
+  children,
+  activeClassName,
+  className,
+  ...props
+}: PropsWithChildren<ActiveLinkProps>) => {
+  const { asPath, isReady } = useRouter();
+  const [computedClassName, setComputedClassName] = useState(className);
+
+  useEffect(() => {
+    if (isReady) {
+      const linkPathname = new URL(
+        (props.as || props.href) as string,
+        location.href,
+      ).pathname;
+
+      const activePathname = new URL(asPath, location.href).pathname;
+
+      const newClassName =
+        linkPathname === activePathname
+          ? `${className} ${activeClassName}`.trim()
+          : className;
+
+      if (newClassName !== computedClassName) {
+        setComputedClassName(newClassName);
+      }
+    }
+  }, [
+    asPath,
+    isReady,
+    props.as,
+    props.href,
+    activeClassName,
+    className,
+    computedClassName,
+  ]);
+
+  return (
+    <Link className={computedClassName} {...props}>
+      {children}
+    </Link>
+  );
+};
+
+export { ActiveLink };
