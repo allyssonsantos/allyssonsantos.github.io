06b8039654c8497f55993495a3a58cb6eceef181
Allysson Santos <aallysson0@gmail.com>
2023-01-03 22:06:12 -0300

    feat: add more information in about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -7,14 +7,14 @@ const Home: NextPage = () => {
     <section>
       <h1 className={styles.about__title}>Hi! I&apos;m Allysson Santos</h1>
       <p>
-        This space is where I test new things that I want to learn and write
-        about thoughts and technology in general but mainly about front-end
-        stuff.
+        This space is where I test new things I want to learn and write about
+        thoughts and technology in general but mainly about front-end stuff.
       </p>
       <hr className={styles.about__divider} />
       <p>
-        I&apos;ve worked with several front-end tools since the basics until the
-        most recent frameworks and libraries, such as: (click to open)
+        I&apos;ve worked with several front-end tools, from the basics to the
+        most recent frameworks and libraries, such as: (click in the language to
+        open)
       </p>
       <details className={styles.about__details}>
         <summary className={styles['about__details--summary']}>HTML</summary>
@@ -160,6 +160,14 @@ const Home: NextPage = () => {
                   <li>NextJS</li>
                 </ul>
               </li>
+              <li>
+                UI Libraries:
+                <ul className={styles.about__list}>
+                  <li>Material UI</li>
+                  <li>Yoga (built by me)</li>
+                  <li>Quantum (built by me)</li>
+                </ul>
+              </li>
             </ul>
           </li>
           <li>React Native</li>
@@ -175,11 +183,11 @@ const Home: NextPage = () => {
         </ul>
       </details>
       <p>
-        Today I&apos;m working with design-systems and microfrontend
+        Today I&apos;m working with design-systems and micro frontends
         implementations.
       </p>
       <hr className={styles.about__divider} />
-      <h2 className={styles.about__subtitle}>My previous experiences:</h2>
+      <h2 className={styles.about__subtitle}>My previous experiences</h2>
       <p>Worked in these companies:</p>
       <ul className={styles.about__list}>
         <li>
@@ -188,9 +196,16 @@ const Home: NextPage = () => {
             <em>2021 - present</em>
           </small>
           <blockquote>
-            Here I&apos;m working with the development of the Olist Design
-            System and also splitting the front-end of the company in small
-            microfrontend applications. (where makes sense)
+            Here I&apos;m working on developing the{' '}
+            <a
+              href="https://designsystem.olist.io/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Olist Design System
+            </a>{' '}
+            and splitting the company&apos;s front-end into small micro
+            frontends applications. (where makes sense)
           </blockquote>
         </li>
         <li>
@@ -198,20 +213,94 @@ const Home: NextPage = () => {
           <small>
             <em>2019 - 2021</em>
           </small>
+          <blockquote>
+            I&apos;ve developed the initial version of the Gympass Wellness
+            product, created the multi-platform{' '}
+            <a
+              href="https://github.com/gympass/yoga"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Yoga design system
+            </a>
+            , and worked with several teams. Here I&apos;ve
+          </blockquote>
         </li>
         <li>
           Catho{' '}
           <small>
             <em>2015 - 2019</em>
           </small>
+          <blockquote>
+            In Catho was where I discovered my passion for the front-end
+            development world. Here I was able to work in so many teams. With
+            different projects and different people, from projects like
+            Salesforce integration or the redesign of the recruiter area to when
+            the company decided to migrate from the legacy PHP coupled
+            applications to SPA&apos;s built with React. I&apos;ve also
+            participated in the company react component library creation called
+            <a
+              href="https://github.com/catho/quantum"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Quantum
+            </a>
+            .
+          </blockquote>
         </li>
         <li>
           Encontra Brasil{' '}
           <small>
             <em>2014 - 2015</em>
           </small>
+          <blockquote>
+            It was a small company where I started to get in touch with the base
+            of HTML, CSS, and Javascript.
+          </blockquote>
         </li>
       </ul>
+      <hr className={styles.about__divider} />
+      <h2 className={styles.about__subtitle}>
+        Projects I&apos;m proud to have been a part of
+      </h2>
+      <div>
+        <h3>Quantum</h3>
+        <p>
+          A react component library that was created in 2018 and is still used
+          nowadays
+        </p>
+      </div>
+      <div>
+        <h3>Yoga</h3>
+        <p>
+          The Gympass multi-platform design-system (React & React-Native), with
+          support for multiple themes and with a great tokenization.
+        </p>
+      </div>
+      <div>
+        <h3>React95</h3>
+        <p>
+          A component library with the Windows 95 UI. With it I started to learn
+          what is and how to handle and maintain a open-source project, here I
+          learned what tree-shaking and semantic-versioning is, that a good
+          documentation and a contribution guide is super important!
+        </p>
+        <p>
+          Also this project gave me a super experience, it was nominated as the
+          &quot;Fun side project of the year&quot; in 2019 at the GitNation open
+          source awards, and it won!!! You can check the moment{' '}
+          <a
+            href="https://youtu.be/4KfAS3zrvX8?t=25484"
+            target="_blank"
+            rel="noreferrer"
+            title="A link to the video of the git nation award where React95 won"
+          >
+            here
+          </a>
+          .
+        </p>
+      </div>
     </section>
   );
 };
