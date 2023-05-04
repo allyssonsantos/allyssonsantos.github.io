ae04d950b5d62811143b860d4453daee6885d37c
Allysson Santos <aallysson0@gmail.com>
2023-05-04 08:52:57 -0300

    feat(blog): add blog feature header style

--- src/features/blog/blog-feature.tsx
+++ src/features/blog/blog-feature.tsx
@@ -2,26 +2,31 @@ import Image from 'next/image';
 import { useMDXComponent } from 'next-contentlayer/hooks';
 import { type Blog } from 'contentlayer/generated';
 
-const H1 = (props: any) => <h1 style={{ backgroundColor: 'red' }} {...props} />;
-
-const components = {
-  h1: H1,
-};
+import styles from './blog.module.css';
 
 export function BlogFeature({ post }: { post: Blog }) {
   const PostBody = useMDXComponent(post.body.code);
+  const readingTime = Math.ceil(post.readingTime.minutes);
 
   return (
     <div>
-      <h1>{post.title}</h1>
-      <small>{post.readingTime.minutes} min</small>
-      <Image
-        src={`/articles/${post.cover}`}
-        alt={post.altCover}
-        width={800}
-        height={600}
-      />
-      <PostBody components={components} />
+      <div className={styles.blog__header}>
+        <h1 className={styles.blog__title}>{post.title}</h1>
+        <small>
+          <abbr title={`estimated time to read: ${readingTime} minute`}>
+            {readingTime} min
+          </abbr>
+        </small>
+      </div>
+      <div className={styles['blog__cover-wrapper']}>
+        <Image
+          className={styles.blog__cover}
+          src={`/articles/${post.cover}`}
+          alt={post.altCover}
+          fill
+        />
+      </div>
+      <PostBody />
     </div>
   );
 }
