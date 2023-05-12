13a36d8d7670a289d54ab5bed7748fa805e8f6ec
Allysson Santos <aallysson0@gmail.com>
2023-05-12 08:58:07 -0300

    feat: export Modal, ModalProvider and useModals

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { ActiveLink } from './ActiveLink';
 export { Button } from './Button';
 export { PostPreview } from './PostPreview';
 export { SideBar } from './SideBar';
+export { Modal, ModalProvider, useModals } from './modal';
