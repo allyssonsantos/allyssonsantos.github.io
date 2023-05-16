313d46ca59fbcf5249dca20f2cc7163deae2856a
Allysson Santos <aallysson0@gmail.com>
2023-05-15 21:40:25 -0300

    feat(components): create alert and textarea

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,5 @@ export { PostPreview } from './PostPreview';
 export { SideBar } from './SideBar';
 export { Modal, ModalProvider, useModals } from './modal';
 export { GoogleButton } from './google-button';
+export { Textarea } from './Textarea';
+export { Alert } from './alert';
