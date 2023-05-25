e627f3b0cbdcab2922a0707dcc55e618715f8c0e
Allysson Santos <aallysson0@gmail.com>
2023-05-24 21:08:20 -0300

    feat(deleteModal): delete user when click in delete button

--- src/components/delete-account-modal/delete-account-modal.tsx
+++ src/components/delete-account-modal/delete-account-modal.tsx
@@ -1,5 +1,8 @@
-import { Button, Modal, useModals } from '../';
 import { DELETE_ACCOUNT_MODAL_KEY } from 'src/constants/modals';
+import { Button, Modal, useModals } from '../';
+import { deleteAccount } from 'src/contexts/auth';
+
+import styles from './delete-account-modal.module.css';
 
 export function DeleteAccountModal() {
   const { openedModals, closeModal } = useModals();
@@ -11,6 +14,11 @@ export function DeleteAccountModal() {
     closeModal(DELETE_ACCOUNT_MODAL_KEY);
   }
 
+  function handleDelete() {
+    deleteAccount();
+    closeModal(DELETE_ACCOUNT_MODAL_KEY);
+  }
+
   return (
     <Modal
       isOpen={isDeleteAccountModalOpen}
@@ -18,10 +26,14 @@ export function DeleteAccountModal() {
       onClose={handleCancel}
     >
       <p>All your comments will be deleted as well.</p>
-      <Button variant="inverted" onClick={handleCancel}>
-        Cancel
-      </Button>
-      <Button>Delete account</Button>
+      <div className={styles.modal__footer}>
+        <Button variant="inverted" onClick={handleCancel}>
+          Cancel
+        </Button>
+        <Button variant="ghost" onClick={handleDelete}>
+          Delete account
+        </Button>
+      </div>
     </Modal>
   );
 }
