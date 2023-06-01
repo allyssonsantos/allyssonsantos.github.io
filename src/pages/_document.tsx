52356d0fa39d023b31a4f17e412fc0defcc95d14
Allysson Santos <aallysson0@gmail.com>
2023-05-31 23:33:06 -0300

    fix: sidebar overflow and add styled-components ssr config

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,47 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  type DocumentContext,
+  type DocumentInitialProps,
+} from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
-import i18nextConfig from '../../next-i18next.config'
+import i18nextConfig from '../../next-i18next.config';
 
 export default class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext,
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
 
   render() {
-    const currentLocale = this.props.__NEXT_DATA__.query.locale || i18nextConfig.i18n.defaultLocale;
+    const currentLocale =
+      this.props.__NEXT_DATA__.query.locale || i18nextConfig.i18n.defaultLocale;
 
     return (
       <Html lang={currentLocale as string}>
