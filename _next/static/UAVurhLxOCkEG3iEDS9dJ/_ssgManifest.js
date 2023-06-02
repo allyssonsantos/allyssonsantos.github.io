6a701e7b4411c8012696ea2418ef81188c2dbe7e
Allysson Santos <aallysson0@gmail.com>
2023-06-02 02:03:23 +0000

    Updates

---
+++ _next/static/UAVurhLxOCkEG3iEDS9dJ/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002F404","\u002F[locale]","\u002F[locale]\u002Fabout","\u002F[locale]\u002Fblog","\u002F[locale]\u002Fblog\u002F[slug]","\u002Fabout","\u002Fblog","\u002Fblog\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
