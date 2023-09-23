1ddc35b14a92ed609aebe5fc9c9f2ca047fa590c
Allysson Santos <aallysson0@gmail.com>
2023-09-23 23:10:40 +0000

    Updates

--- _next/static/chunks/pages/[locale]/about-be2e1863b70760fe.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[721],{815:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/[locale]/about",function(){return u(1227)}])}},function(n){n.O(0,[227,774,888,179],function(){return n(n.s=815)}),_N_E=n.O()}]);
