0e2c3bfce32589b55cc7fe1bd11aee93050465b9
Allysson Santos <aallysson0@gmail.com>
2024-06-13 22:07:07 -0300

    feat(sidebar): disable language switcher option if article is not available

--- src/features/home/home-feature.tsx
+++ src/features/home/home-feature.tsx
@@ -3,7 +3,7 @@ import { format, parseISO } from 'date-fns';
 import { useTranslation } from 'next-i18next';
 import type { Blog } from 'contentlayer/generated';
 
-import { PostPreview, CodeEditor } from 'src/components';
+import { PostPreview } from 'src/components';
 import styles from './home.module.css';
 
 type HomeFeatureProps = {
