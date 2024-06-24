f82d9e09833c44e03f3c6a8693052fbb1356b4d2
Allysson Santos <allysson.s@grupoboticario.com.br>
2024-06-24 08:55:43 -0300

    fix(contentlayer): use contentlayer2 in build npm script

--- src/features/article/components/PostBody.tsx
+++ src/features/article/components/PostBody.tsx
@@ -1,7 +1,7 @@
 import { useMDXComponent } from 'next-contentlayer2/hooks';
 import { CodeEditor, GoToFile } from 'src/components/code-editor/code-editor';
 
-export function PostBody({ code }: { code: string }) {
+export function PostBody({ code }: Readonly<{ code: string }>) {
   const Body = useMDXComponent(code);
 
   return (
